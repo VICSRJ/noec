74d0ca1bf89698711a1c1ce4c642ecb80cc0257c
seethreepeeou-pixel <seethreepeeou@gmail.com>
2026-09-03 07:17:06 +0200

    fix: make robots metadata route static-export safe

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,11 @@
 import { baseURL } from "@/resources";
 
+export const dynamic = "force-static";
+export const revalidate = false;
+
 export default function robots() {
   return {
-    rules: [
-      {
-        userAgent: "*",
-      },
-    ],
+    rules: [{ userAgent: "*" }],
     sitemap: `${baseURL}/sitemap.xml`,
   };
 }
