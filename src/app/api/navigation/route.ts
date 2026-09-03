ceb35b322485267795b315717d696b815ff782d3
seethreepeeou-pixel <seethreepeeou@gmail.com>
2026-09-03 07:18:48 +0200

    refactor: remove runtime navigation API from static site

--- src/app/api/navigation/route.ts
+++
@@ -1,9 +0,0 @@
-import getNavigation from "@/app/utils/getNavigation";
-import { NextResponse } from "next/server";
-
-export const dynamic = "force-static";
-export const revalidate = false;
-
-export async function GET() {
-  return NextResponse.json(getNavigation());
-}
